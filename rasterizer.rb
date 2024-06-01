6e25ca1b2a26dc4cf011c6b47fe2567c873e4b60
Gabi Jack <gabi@gabijack.com>
2024-06-01 09:38:13 -0400

    Add a method to draw a wireframe triangle by drawing a line from point to point. Add a method to draw a filled triangle as a series of horizontal lines going from edge to edge of the triangle for each value of y between the lowest point of the triangle to the highest. The edges of the filled triangle won't always match perfectly to the edges of the wireframe triangle because we are approximating

--- rasterizer.rb
+++ rasterizer.rb
@@ -32,8 +32,6 @@ class Rasterizer
         canvas.put_pixel(x_values[y - point1[1]], y, color)
       end
     end
-
-    canvas.save_image(filename: "example1.bmp")
   end
 
   # we know the slope and we know all points in a line
@@ -56,6 +54,52 @@ class Rasterizer
       values << d
       d = d + a
     end
-    values
+    values.map(&:floor)
+  end
+
+  def draw_wireframe_triangle(point0, point1, point2, color)
+    draw_line(point0, point1, color)
+    draw_line(point1, point2, color)
+    draw_line(point2, point0, color)
+  end
+
+  def draw_filled_triangle(point0, point1, point2, color)
+    # sort the points so that point0 < point1 < point2
+    if point1[1] < point0[1]
+      point0, point1 = point1, point0
+    end
+    if point2[1] < point0[1]
+      point0, point2 = point2, point0
+    end
+    if point2[1] < point1[1]
+      point1, point2 = point2, point1
+    end
+
+    # compute the x coordinates of the triangle edges
+    x01 = interpolate(point0[1], point0[0], point1[1], point1[0])
+    x12 = interpolate(point1[1], point1[0], point2[1], point2[0])
+    x02 = interpolate(point0[1], point0[0], point2[1], point2[0])
+
+    # concatenate the short sides
+    x01.pop
+    x012 = x01 + x12
+
+    # determine which is left and which is right
+    m = (x012.length / 2.0).floor
+
+    if x02[m] < x012[m]
+      x_left = x02
+      x_right = x012
+    else
+      x_left = x012
+      x_right = x02
+    end
+
+    # draw the horizontal segments 
+    (point0[1]..point2[1]).each do |y|
+      (x_left[y - point0[1]]..x_right[y - point0[1]]).each do |x|
+        canvas.put_pixel(x, y, color)
+      end
+    end
   end
 end
