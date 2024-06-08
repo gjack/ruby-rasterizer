b0aa7c905a0e1bfd6eb75ec473ed111053cf9bd3
Gabi Jack <gabi@gabijack.com>
2024-06-08 10:00:32 -0400

    Allow camera to receive an orientation_angle and have transformations of its own.
    Use the origin to obtain a translation matrix and the orientation_angle to obtain a rotation_matrix. Apply the algorithm to find the resulting transformation matrix regarding the scene.

--- camera.rb
+++ camera.rb
@@ -1,9 +1,17 @@
+require_relative "transformable"
+
 class Camera
-    attr_reader :origin, :distance
+  include Transformable
+  attr_reader :origin, :distance, :orientation_angle
   
-    # initialize with origin and distance to projection plane
-    def initialize(origin: [0,0,0], distance: 1.0)
-      @origin = origin
-      @distance = distance
-    end
+  # initialize with origin and distance to projection plane
+  def initialize(origin: [0,0,0], distance: 1.0, orientation_angle: nil)
+    @origin = origin
+    @distance = distance
+    @orientation_angle = orientation_angle
   end
+
+  def transformations
+    multiply_44matrices(transposed_matrix(rotation_matrix(orientation_angle)), translation_matrix(multiply_by_scalar(origin, -1)))
+  end
+end
